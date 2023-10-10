8c92bc30f6a56b9db0b24eab8b55591c106739cc
astianmuchui <astianmuchui@gmail.com>
2023-10-10 13:32:33 +0300

    base model

--- models/base_model.py
+++ models/base_model.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 import uuid
 from datetime import datetime
 class BaseModel():
@@ -7,10 +8,14 @@ class BaseModel():
         This is the base model for the HBNB Project
         To be extended by the other classes
     """
-    def __str__(self):
+
+    def __str__(self, *args, **kwargs):
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs is not None:
+            pass
         return "[{}] ({}) {}".format(self.__class__.__name__, self.id, self.__dict__)
     pass
 
@@ -19,5 +24,10 @@ class BaseModel():
     pass
 
     def to_dict(self):
-        pass
+        self.created_at = datetime.isoformat(self.created_at)
+        self.updated_at = datetime.isoformat(self.updated_at)
+        temp = self.__dict__.copy()
+        temp["__class__"] = self.__class__.__name__
 
+        return temp
+    pass
