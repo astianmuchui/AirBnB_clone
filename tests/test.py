4dc25889e852b0dfae70c2b3bc577e7ccdc75472
astianmuchui <astianmuchui@gmail.com>
2023-10-10 12:23:37 +0300

    Instances for the basemodel as per the provided test suite can be found here

---
+++ tests/test.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+from models.base_model import BaseModel
+
+my_model = BaseModel()
+my_model.name = "My First Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
+print(my_model)
