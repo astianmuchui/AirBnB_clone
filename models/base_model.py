9750b37ed009eca015eb0275a172efda41ef7e0c
astianmuchui <astianmuchui@gmail.com>
2023-10-11 13:28:25 +0300

    Worked on task 3 and 4

--- models/base_model.py
+++ models/base_model.py
@@ -14,6 +14,15 @@ class BaseModel():
         self.id = str(uuid.uuid4())
         self.created_at = datetime.now()
         self.updated_at = datetime.now()
+
+        if kwargs:
+            keys = kwargs.keys()
+
+            for key in keys:
+                if key == "created_at" or key == "updated_at":
+                    kwargs[key] = str(datetime.strptime(kwargs[key], "%Y-%m-%dT%H:%M:%S.%f"))
+                if key != "__class__":
+                    setattr(self, key, kwargs[key])
     pass
 
     def __str__(self):
