acab8e190a82a7bd35537f5b243e09af0d5e0179
astianmuchui <astianmuchui@gmail.com>
2023-10-15 11:25:26 +0300

    test

--- test.py
+++ test.py
@@ -1,2 +1,17 @@
 #!/usr/bin/python3
 
+from models import storage
+from models.base_model import BaseModel
+
+all_objs = storage.all()
+print("-- Reloaded objects --")
+for obj_id in all_objs.keys():
+    obj = all_objs[obj_id]
+    print(obj.to_dict())
+
+print("-- Create a new object --")
+my_model = BaseModel()
+my_model.name = "My_First_Model"
+my_model.my_number = 89
+print(my_model)
+my_model.save()
