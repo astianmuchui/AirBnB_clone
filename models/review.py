4f63640fed8ec25152356eb82696ac39587af00b
astianmuchui <astianmuchui@gmail.com>
2023-10-16 06:07:14 +0300

    final trials

--- models/review.py
+++ models/review.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 """Review Module"""
-
+from models.base_model import BaseModel
 class Review(BaseModel):
         """Review class inherits from BaseModel"""
         place_id = ""
